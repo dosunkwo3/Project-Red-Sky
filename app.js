84afe17c619a6c8aca3a8644b66ff3006880aaeb
Kevin Hudson <khudso30@uncc.edu>
2022-12-08 20:22:58 -0500

    regions working

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //require modules
 const express = require('express');
+const path = require('path');
 const morgan = require('morgan');
 const ejs = require('ejs');
 
@@ -13,21 +14,22 @@ app.set('view engine', 'ejs');
 
 //mount middleware
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('tiny'));
-
 //set up routes
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
     res.render('index');
 });
 
+
+
 //load charts page
-app.get('/charts', (req, res)=> {
+app.get('/charts', (req, res) => {
     res.render('charts');
 })
 
 //start the server
-app.listen(port, host, () =>{
+app.listen(port, host, () => {
     console.log('The server is running at port ', port);
 });
 
